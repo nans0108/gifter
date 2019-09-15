39abe7e18d8352e804ea641d51a989c00a5ca334
Sara Rosicki <rosicki.1995@gmail.com>
2019-09-15 17:46:56 +0200

    quick fix

--- store.js
+++ store.js
@@ -1,12 +1,17 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import listReducer from './reducers/listReducer';
+import createRequestMiddleware  from './middlewares/requestMiddleware';
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
   lists: listReducer
 });
 
 const configureStore = () => {
-  return createStore(rootReducer);
+  return createStore(rootReducer, applyMiddleware(
+    thunk,
+    createRequestMiddleware(),
+  ));
 }
 
 export default configureStore;
